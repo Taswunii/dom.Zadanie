728f28873e53753404ad995e25a38999c04b8ab8
USER - PC <xgcyjsxgcgs@gmail.com>
2024-01-27 10:54:46 +0200

    v1.2

--- main.py
+++ main.py
@@ -1,30 +1,60 @@
-class Car:
-    mark = "Audi"
-    model = "gt rs"
-    year = "2023"
-
-    def __init__(self):
-        self.name = "E-tron"
-        self.hp = 600
+import random
 
 
-cr = Car()
+class Student:
 
-print(cr.mark, cr.name, cr.model, cr.hp, "hp")
+    def __init__(self, name):
+        self.name = name
+        self.gladness = 50
+        self.progress = 0
+        self.alive = True
+
+    def to_study(self):
+        print('Time to study')
+        self.progress += 0.15
+        self.gladness -= 3
+
+    def to_sleep(self):
+        print("I will sleep")
+        self.gladness += 3
+
+    def to_chill(self):
+        print("Rest time")
+        self.gladness += 5
+        self.progress -= 0.1
+
+    def is_alive(self):
+        if self.progress < -0.5:
+            print("Cast out...")
+            self.alive = False
+        elif self.gladness <= 0:
+            print("Depression...")
+            self.alive = False
+        elif self.progress > 5:
+            print("Passed externally...")
+            self.alive = False
+
+    def end_of_day(self):
+        print(f"Gladness = {self.gladness}")
+        print(f"Progress = {round(self.progress, 1)}")
+
+    def live(self, day):
+        d = "Day" + str(day) + " of " + self.name + " life"
+        print(f"{day:=^50}")
+        live_cube = random.randint(1, 3)
+        if live_cube == 1:
+            self.to_study()
+        elif live_cube == 2:
+            self.to_sleep()
+        elif live_cube == 3:
+            self.to_chill()
+        self.end_of_day()
+        self.is_alive()
 
 
-
-class Animal:
-    group = "mammal"
-    tipe = "predator"
-    eats = "omnivorous"
-
-    def __init__(self):
-        self.name = "lion"
-        self.height = 190
-
-
-an = Animal()
-
-print(an.group, an.tipe, an.eats, an.name, an.height, "kg")
-
+name = input("Input name ")
+nick = Student(name=name)
+for i in range(1, 365):
+    if nick.alive == False:
+        break
+    nick.live(i)

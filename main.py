29785bf2a0519f0c3c3e771ff75ae722300652ed
USER - PC <xgcyjsxgcgs@gmail.com>
2024-02-02 19:11:42 +0200

    v1.4

--- main.py
+++ main.py
@@ -1,49 +1,49 @@
 import random
 
 
-class Student:
-
+class Dog:
     def __init__(self, name):
         self.name = name
         self.gladness = 50
-        self.progress = 0
+        self.height = 20
         self.alive = True
 
-    def to_study(self):
+    def eating(self):
         print('Time to study')
-        self.progress += 0.15
-        self.gladness -= 3
+        self.gladness += 3
+        self.height += 0.25
+
 
     def to_sleep(self):
         print("I will sleep")
         self.gladness += 3
-
+        self.height -= 0.10
     def to_chill(self):
         print("Rest time")
         self.gladness += 5
-        self.progress -= 0.1
+        self.height -= 0.1
 
     def is_alive(self):
-        if self.progress < -0.5:
-            print("Cast out...")
+        if self.height < 7:
+            print("Lack of weight...")
             self.alive = False
         elif self.gladness <= 0:
             print("Depression...")
             self.alive = False
-        elif self.progress > 5:
-            print("Passed externally...")
+        elif self.height > 45:
+            print("Obesity...")
             self.alive = False
 
     def end_of_day(self):
         print(f"Gladness = {self.gladness}")
-        print(f"Progress = {round(self.progress, 1)}")
+        print(f"Height = {round(self.height, 1)}")
 
     def live(self, day):
         d = "Day" + str(day) + " of " + self.name + " life"
         print(f"{day:=^50}")
         live_cube = random.randint(1, 3)
         if live_cube == 1:
-            self.to_study()
+            self.eating()
         elif live_cube == 2:
             self.to_sleep()
         elif live_cube == 3:
@@ -53,7 +53,7 @@ class Student:
 
 
 name = input("Input name ")
-nick = Student(name=name)
+nick = Dog(name=name)
 for i in range(1, 365):
     if nick.alive == False:
         break
